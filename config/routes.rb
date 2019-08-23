414c7206178a8f62f029a0d981039483aaf0e844
aqilahrajab <YOUR-EMAIL-ADDRESS>
2019-08-23 10:50:21 +0800

    Created the landing page git added - 1. assets - styling and image 2. Home controllers 3. Views 4. Updated routes (within include freelancers routes as well)

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
   #root page
-  root 'freelancers#register_expertise'
+  root 'homes#index'
 
-  #http://localhost:3000/freelancers/register_expertise
   get '/freelancers/register_expertise' => 'freelancers#register_expertise', as: 'freelancer_register_expertise'
-
-  #http://localhost:3000/create_expertise
   post '/freelancer_create_expertise' => 'freelancers#create_expertise'
-
   get '/freelancers/register_profile' => 'freelancers#register_profile', as: 'freelancer_register_profile'
   post '/freelancer_create_profile' => 'freelancers#create_profile'
-  
-
-    # Show - Freelancer
-    get '/freelancers/:id' => 'freelancers#show' , as: 'freelancer'
+  get '/freelancers/:id' => 'freelancers#show' , as: 'freelancer'
 
 end
